8b747d9a63214f1f3cc3c58a8741a5aa4219e855
DaniJani2025 <dhananjaywork2025@gmail.com>
2026-03-09 00:19:06 +0530

    standardize vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import type { ConfigEnv } from "vite";
 
-export default ({ mode }: ConfigEnv) => {
-  const env = loadEnv(mode, process.cwd());
-
-  const https =
-    env.VITE_USE_HTTPS === "true"
-      ? {
-          key: fs.readFileSync(env.VITE_HTTPS_KEY_PATH),
-          cert: fs.readFileSync(env.VITE_HTTPS_CERT_PATH)
-        }
-      : undefined;
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      host: mode === "lan" ? true : "localhost",
-      https
-    }
-  });
-};
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 5173
+  }
+});
